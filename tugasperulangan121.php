8482690050607774a9da9e334cc76ce6d2d68c31
nadzimfawwaz321 <e42241971@student.polije.ac.id>
2025-09-30 13:35:45 +0700

    latihan_PHP

---
+++ tugasperulangan121.php
@@ -0,0 +1,9 @@
+<?php
+// Program PHP untuk menghitung dari 100 sampai 1000
+
+for ($i = 100; $i <= 1000; $i++) {
+    echo $i . "\n"; // Menggunakan "\n" untuk baris baru dalam mode konsol/teks
+    // Atau: echo $i . "<br>"; // Menggunakan "<br>" jika dijalankan di browser web
+}
+
+?>
